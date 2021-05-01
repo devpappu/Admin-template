8d578df545c0476f9ed321d64461059737e86e71
Roobnet Pappu <81929129+DEV-PAPPU@users.noreply.github.com>
2021-05-01 20:53:28 +0600

    Add html,Css Files For Admin Template

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+$("#icon-menu").click(function() {
+    $(".admin").toggleClass("menu-setting");
+});
+
+
+// $(".side-nav-menu").hover(function() {
+//     $(".side-nav-menu").toggleClass("menu-hover");
+// });
+
+
+
+$("#p").click(function() {
+    console.log("click")
+
+});
